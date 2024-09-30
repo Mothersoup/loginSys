f13b1e86f481b288a7f07cc6186017791ff10cb5
Borlon <66214085+Mothersoup@users.noreply.github.com>
2024-09-30 14:18:44 +0800

    Add files via upload

--- SignUpController.java
+++ SignUpController.java
@@ -1,11 +1,13 @@
 package com.example.loginsystem;
 
 
+import com.example.loginsystem.model.dto.SignUpDTO;
 import com.example.loginsystem.model.entity.User;
 import com.example.loginsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
@@ -16,18 +18,22 @@ import java.time.LocalDateTime;
 public class SignUpController {
 
     private final UserService userService;
-
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public SignUpController(UserService userService) {
+    public SignUpController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @RequestMapping(value = "/signUp", method = RequestMethod.POST)
-    public ResponseEntity<String> registerStudent(@RequestBody User user) {
-        if (userService.checkIfEmailExists(user.getEmail())) {
+    public ResponseEntity<String> registerStudent(@RequestBody SignUpDTO signUpDTO ) {
+        if (userService.checkIfEmailExists( signUpDTO.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already in use");
         }
+        User user = new User();
         // setting create time
+        user.setEmail( signUpDTO.getEmail() );
+        user.setPassword( passwordEncoder.encode( signUpDTO.getPassword()) );
         LocalDateTime localDateTime = LocalDateTime.now();
         Timestamp createTime = Timestamp.valueOf(localDateTime);
         user.setCreatedAt( createTime );
